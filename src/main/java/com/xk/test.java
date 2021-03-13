cd2ff627a1897fc033ae71bcfb4d89dab614f559
xk <1548051137@qq.com>
2021-03-13 23:23:25 +0800

    start

---
+++ src/main/java/com/xk/test.java
@@ -0,0 +1,12 @@
+package com.xk;
+
+public class test {
+
+    public static void main(String[] args) {
+        short s=1;
+        int g = 1;
+        double sss=11;
+        long ss=s;
+
+    }
+}
